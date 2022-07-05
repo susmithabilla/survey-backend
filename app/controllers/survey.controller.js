246052c20f9dafc08d79cdcbeceb3bad1a4acda3
AmruthaReddy97 <a.balasaani@eagles.oc.edu>
2022-07-05 01:07:33 -0500

    find all, update,delete survey apis added

--- app/controllers/survey.controller.js
+++ app/controllers/survey.controller.js
@@ -67,3 +67,70 @@ exports.findOne = (req, res) => {
       });
     });
 };
+ // Retrieve all surveys from the db.
+ exports.findAll = (req, res) => {
+  const title = req.query.title;
+  var condition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  Survey.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "error occurred while retrieving surveys."
+      });
+    });
+};
+// Update a survey by the id in the request
+exports.update = (req, res) => {
+
+  const survey = {
+    title: req.query.title!=undefined ? req.query.title:null,
+    userId: req.query.userId!=undefined ? req.query.userId:null,
+    data:req.body!=undefined? req.body:null,  
+  };
+  const id = req.params.id;
+  Survey.update(survey, {
+    where: { id: id } 
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Survey was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update Survey with id=${id}. Maybe Survey was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating Survey with id=" + id
+      });
+    });
+};
+// Delete a Survey with the specified id in the request
+exports.delete = (req, res) => {
+  const id = req.params.id;
+  Survey.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Survey was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete Survey with id=${id}. Maybe Survey was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete Survey with id=" + id
+      });
+    });
+};
